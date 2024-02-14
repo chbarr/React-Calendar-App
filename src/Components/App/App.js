c0a3f525aaa781ef31746b2c63f887ef95abbc80
christianbarr <cbarreto@mbp.com.co>
2024-02-13 22:17:34 -0500

    fixed customHook useCalendar to return an object with 35 days in total, counting last month days, current month days and next month days, and display them in order

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,39 @@
 import './App.css';
-import { useCalendar } from '../../customHooks/useCalendar';
+import { useCalendar, weekDaysES, monthsES } from '../../customHooks/useCalendar';
 function App() {
   const days = useCalendar();
   return (
     <div className="App">
-      <span> Hola hola hola</span>
-      <span>{JSON.stringify(days)}</span>
-    </div>
+      <div>
+        <h2>{monthsES[days.month]}</h2>
+        <div className='calendarGridHeader'>
+          {weekDaysES.slice(0, weekDaysES.length - 1).map((day, i) => (
+            <div key={i}
+              className="dayCellHeader">
+              {day.slice(0, 3).toUpperCase()}
+            </div>
+          ))
+          }
+        </div>
+        <div className='calendarGrid'>
+          {
+            days['previousMonth'].map(dayNumber => (
+              <div className="dayCell">{dayNumber}</div>
+            ))
+          }
+          {
+            days['currentMonth'].map((_, i) => (
+              <div className="dayCell">{i + 1}</div>
+            ))
+          }
+          {
+            days['nextMonth'].map((_, i) => (
+              <div className="dayCell">{i + 1}</div>
+            ))
+          }
+        </div>
+      </div>
+    </div >
   );
 }
 
--- src/customHooks/useCalendar.js
+++ src/customHooks/useCalendar.js
@@ -1,15 +1,40 @@
-function useCalendar(year = new Date().getFullYear(), month = new Date().getMonth()){
-    const lastMonthDay = new Date(year, month + 1, 0);
+export const weekDaysES = ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+export const monthsES = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
 
-    const monthDays = lastMonthDay.getDate();
+const getPreviousMonth = (currentMonth) => {
+    return (currentMonth === 0 ? 11 : currentMonth - 1);
+}
 
-    const days = [];
-    for (let day = 1; day <= monthDays; day++){
-        const currentDate  = new Date(year, month, day);
+const getNextMonth = (currentMonth) => {
+    return (currentMonth === 11 ? 0 : currentMonth + 1);
+}
+
+function useCalendar(year = new Date().getFullYear(), month = new Date().getMonth()) {
+    const lastMonthDay = new Date(year, getNextMonth(month), 0);
+
+    const days = {
+        'month': month,
+        'previousMonth': [],
+        'currentMonth': [],
+        'nextMonth': []
+    };
+
+    let monthDays = lastMonthDay.getDate();
+
+    for (let day = 1; day <= monthDays; day++) {
+        const currentDate = new Date(year, month, day);
         const weekDay = currentDate.toLocaleDateString('es-ES', { weekday: 'long' });
-        days.push(day, weekDay);
+        days['currentMonth'].push(weekDay);
+    }
+    if (days['currentMonth'][0].toLowerCase() !== weekDaysES[0].toLowerCase()) {
+        monthDays = new Date(year, getNextMonth(getPreviousMonth(month)), 0);
+        const previousMonthLastDays = weekDaysES.slice(0, weekDaysES.indexOf(days['currentMonth'][0].toLowerCase()));
+        previousMonthLastDays.forEach((_, i) => days['previousMonth'].push(monthDays.getDate() - (previousMonthLastDays.length - 1) + i));
+    }
+    if (days['currentMonth'][days['currentMonth'].length - 1].toLowerCase() !== weekDaysES[0].toLowerCase()) {
+        days['nextMonth'] = weekDaysES.slice(weekDaysES.indexOf(days['currentMonth'][days['currentMonth'].length - 1].toLowerCase()) + 2);
     }
     return days;
 }
 
-export {useCalendar}
+export { useCalendar }
