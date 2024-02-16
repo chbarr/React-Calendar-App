9ed48f15839ad879ad85817d94e38cd1b006da81
chbarr <=>
2024-02-16 00:41:32 -0500

    useCalendar custom hook fixed to display all year months days correctly

--- src/customHooks/useCalendar.js
+++ src/customHooks/useCalendar.js
@@ -1,20 +1,19 @@
 import { useSelector } from "react-redux";
 
-export const weekDaysES = ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+export const weekDaysES = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
 export const monthsNameES = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
-const getPreviousMonth = (currentMonth) => {
-    return (currentMonth === 0 ? 11 : currentMonth - 1);
+const getPreviousMonth = (currentYear, currentMonth) => {
+    return (currentMonth === 0) ? [currentYear - 1, 0] : [currentYear, currentMonth - 1];
 }
 
-const getNextMonth = (currentMonth) => {
-    return (currentMonth === 11 ? 0 : currentMonth + 1);
-}
+const getNextMonth = (currentYear, currentMonth) => {
+    return (currentMonth === 11) ? [currentYear + 1, 0] : [currentYear, currentMonth + 1];
+};
 
 function useCalendar() {
     const [year, month] = useSelector(state => [state.ui.selectedYear, state.ui.selectedMonth]);
-    const lastMonthDay = new Date(year, getNextMonth(month), 0);
-
+    
     const days = {
         'month': month,
         'year': year,
@@ -22,21 +21,23 @@ function useCalendar() {
         'currentMonth': [],
         'nextMonth': []
     };
+    
+    //obtains the next month to build a date object with the current month last day
+    const [nextYear, nextMonth] = getNextMonth(year, month);
+    const currentMonthLastDay = new Date(nextYear, nextMonth, 0).getDate();
 
-    let monthDays = lastMonthDay.getDate();
-
-    for (let day = 1; day <= monthDays; day++) {
+    for (let day = 1; day <= currentMonthLastDay; day++) {
         const currentDate = new Date(year, month, day);
         const weekDay = currentDate.toLocaleDateString('es-ES', { weekday: 'long' });
         days['currentMonth'].push(weekDay);
     }
     if (days['currentMonth'][0].toLowerCase() !== weekDaysES[0].toLowerCase()) {
-        monthDays = new Date(year, getNextMonth(getPreviousMonth(month)), 0);
+        const previousMonthLastDay = new Date(year, month, 0).getDate();
         const previousMonthLastDays = weekDaysES.slice(0, weekDaysES.indexOf(days['currentMonth'][0].toLowerCase()));
-        previousMonthLastDays.forEach((_, i) => days['previousMonth'].push(monthDays.getDate() - (previousMonthLastDays.length - 1) + i));
+        previousMonthLastDays.forEach((_, i) => days['previousMonth'].push(previousMonthLastDay - (previousMonthLastDays.length - 1) + i));
     }
-    if (days['currentMonth'][days['currentMonth'].length - 1].toLowerCase() !== weekDaysES[0].toLowerCase()) {
-        days['nextMonth'] = weekDaysES.slice(weekDaysES.indexOf(days['currentMonth'][days['currentMonth'].length - 1].toLowerCase()) + 2);
+    if (days['currentMonth'][currentMonthLastDay - 1].toLowerCase() !== weekDaysES[6].toLowerCase()) {
+        days['nextMonth'] = weekDaysES.slice(weekDaysES.indexOf(days.currentMonth[currentMonthLastDay-1]) + 2);
     }
     return days;
 }
