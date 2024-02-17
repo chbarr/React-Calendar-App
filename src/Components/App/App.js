e4ac43ef5bf1ceaa833742970c8c03ceeae3b2c3
chbarr <=>
2024-02-17 11:31:34 -0500

    created Calendar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,38 +1,12 @@
 import './App.css';
-import { useCalendar, weekDaysES } from '../../customHooks/useCalendar';
 import { DateSelector } from '../DateSelector/DateSelector';
+import Calendar from '../Calendar/Calendar';
 function App() {
-  const calendar = useCalendar();
   return (
     <div className="App">
       <div>
         <DateSelector />
-        <div className='calendarGridHeader'>
-          {weekDaysES.slice(0, weekDaysES.length - 1).map((day, i) => (
-            <div key={i}
-              className="dayCellHeader">
-              {day.slice(0, 3).toUpperCase()}
-            </div>
-          ))
-          }
-        </div>
-        <div className='calendarGrid'>
-          {
-            calendar['previousMonth'].map(dayNumber => (
-              <div className="dayCell">{dayNumber}</div>
-            ))
-          }
-          {
-            calendar['currentMonth'].map((_, i) => (
-              <div className="dayCell">{i + 1}</div>
-            ))
-          }
-          {
-            calendar['nextMonth'].map((_, i) => (
-              <div className="dayCell">{i + 1}</div>
-            ))
-          }
-        </div>
+        <Calendar/>
       </div>
     </div >
   );
---
+++ src/Components/Calendar/Calendar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './calendar.css'
+import { useCalendar, weekDaysES } from '../../customHooks/useCalendar';
+
+function Calendar() {
+    const calendar = useCalendar();
+    return (
+        <section className='calendar-container'>
+            <div className='calendarGridHeader'>
+                {weekDaysES.slice(0, weekDaysES.length - 1).map((day, i) => (
+                    <div key={i}
+                        className="dayCellHeader">
+                        {day.slice(0, 3).toUpperCase()}
+                    </div>
+                ))
+                }
+            </div>
+            <div className='calendarGrid'>
+                {
+                    calendar['previousMonth'].map(dayNumber => (
+                        <div className="dayCell">{dayNumber}</div>
+                    ))
+                }
+                {
+                    calendar['currentMonth'].map((_, i) => (
+                        <div className="dayCell">{i + 1}</div>
+                    ))
+                }
+                {
+                    calendar['nextMonth'].map((_, i) => (
+                        <div className="dayCell">{i + 1}</div>
+                    ))
+                }
+            </div>
+        </section>
+    )
+}
+
+export default Calendar
